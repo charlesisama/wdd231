ea4e3623d0197e8ca710ceda87e4f42df1e6ae5f
charlesisama <cisama@byupathway.edu>
2026-03-10 08:49:56 +0100

    week01 assignment: course page

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// SET CURRENT DATE AND LAST MODIFICATION
+const yearSpan = document.getElementById("currentyear");
+
+const lastModified = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear ();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
